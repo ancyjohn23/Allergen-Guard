96d38d9bbf4ed3e94125e9ebd685d1e74e77d1dd
ancyjohn23 <131434134+ancyjohn23@users.noreply.github.com>
2024-04-16 11:47:07 +0530

    Add files via upload

---
+++ allergens.js
@@ -0,0 +1,40 @@
+const allergens = [{
+        name: 'dairy',
+        data: ["milk", "cheese", "chedder", "mozzarella", "cream", "dairy", "dairy product", "brie", "gouda", "tortellini", "ravioli", "parmesan"]
+    },
+    {
+        name: 'eggs',
+        data: ["egg", "mayonnaise", "hollandaise", "hollandaise sauce", "cake", "pie", "pasta"]
+    },
+    {
+        name: 'peanuts',
+        data: ["nut", "peanut"]
+    },
+    {
+        name: 'treenuts',
+        data: ["nut", "almond", "peanut", "cashew", "hazelnut", "pistachio", "pine nut", "peacan"]
+    },
+    {
+        name: 'soy',
+        data: ["edamame", "soy beans", "soy nut", "tofu"]
+    },
+    {
+        name: 'wheat',
+        data: ["tortilla", "bread", "wheat", "pastry", "dough", "bun", "cereal", "flour", "doughnut", "bread rolls", "crust", "brioche", "rye", "croissant", "bagel", "sourdough bread", "cake", "french bread", "yeast", "taco", "toast", "pizza", "pie", "pasta", "macaroni", "spaghetti", "penne", "tagliatelle", "fettuccine", "tortellini", "sandwich", "angel-hair pasta", "ravioli"]
+    },
+    {
+        name: 'fish',
+        data: ["fish", "salmon", "tuna", "smoked salmon", "lox", "sashimi", "sushi", "seafood"]
+    },
+    {
+        name: 'shellfish',
+        data: ["shellfish", "crab", "shrimp", "lobster", "prawn", "mussel", "clam", "crayfish", "scampi", "sushi", "seafood"]
+    },
+    {
+        name: 'corn',
+        data: ["corn", "tortilla", "taco"]
+    }
+
+];
+
+// module.exports = allergens;
